15390b988817ab02a383c6de535c9022f934e124
JJ Grunwald <113119782+jjgrunwald@users.noreply.github.com>
2024-08-05 19:41:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import * as THREE from '/build/three.module.js';
 
-import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+import {OrbitControls} from '/examples/jsm/controls/OrbitControls.js';
 
 
 class BasicWorldDemo {
